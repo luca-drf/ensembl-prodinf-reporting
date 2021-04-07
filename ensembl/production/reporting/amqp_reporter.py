e35e319e6df66b7b5ecae1fd6da7d1dd886249e9
Luca Da Rin Fioretto <ldrf@ebi.ac.uk>
2021-04-07 14:36:45 +0100

    Add Black and reformat

--- ensembl/production/reporting/amqp_reporter.py
+++ ensembl/production/reporting/amqp_reporter.py
@@ -12,11 +12,13 @@ from ensembl.production.reporting.config import config
 
 
 LOG_LEVEL = logging.DEBUG if config.debug else logging.INFO
-logging.basicConfig(stream=sys.stdout,
-                    format="%(asctime)s %(levelname)-8s %(name)-15s --: %(message)s",
-                    level=LOG_LEVEL)
+logging.basicConfig(
+    stream=sys.stdout,
+    format="%(asctime)s %(levelname)-8s %(name)-15s --: %(message)s",
+    level=LOG_LEVEL,
+)
 
-logger = logging.getLogger('amqp_reporter')
+logger = logging.getLogger("amqp_reporter")
 
 
 queue = Queue(config.amqp_queue)
@@ -26,31 +28,34 @@ hub = Hub()
 
 @contextmanager
 def es_reporter():
-    es = Elasticsearch([{'host': config.es_host, 'port': config.es_port}])
+    es = Elasticsearch([{"host": config.es_host, "port": config.es_port}])
 
     def on_message(message: Message):
-        logger.debug('From queue: %s, received: %s',
-                      config.amqp_queue, message.body)
+        logger.debug("From queue: %s, received: %s", config.amqp_queue, message.body)
         try:
             json.loads(message.body)
         except json.JSONDecodeError as err:
-            logger.critical('Cannot decode JSON message: %s', message.body)
+            logger.critical("Cannot decode JSON message: %s", message.body)
             message.reject()
-            logger.debug('Rejected: %s', message.body)
+            logger.debug("Rejected: %s", message.body)
             return
         try:
-            es.index(index=config.es_index,
-                     body=message.body,
-                     doc_type=config.es_doc_type)
+            es.index(
+                index=config.es_index, body=message.body, doc_type=config.es_doc_type
+            )
         except ElasticsearchException as err:
-            logger.critical('Cannot modify index %s. Error: %s', config.es_index, err)
+            logger.critical("Cannot modify index %s. Error: %s", config.es_index, err)
             message.requeue()
-            logger.debug('Requeued: %s', message.body)
+            logger.debug("Requeued: %s", message.body)
             return
-        logger.debug('To index: %s, type: %s, document: %s',
-                      config.es_index, config.es_doc_type, message.body)
+        logger.debug(
+            "To index: %s, type: %s, document: %s",
+            config.es_index,
+            config.es_doc_type,
+            message.body,
+        )
         message.ack()
-        logger.debug('Acked: %s', message.body)
+        logger.debug("Acked: %s", message.body)
 
     try:
         yield on_message
@@ -60,10 +65,10 @@ def es_reporter():
 
 def compose_email(email: dict) -> EmailMessage:
     msg = EmailMessage()
-    msg['Subject'] = email['subject']
-    msg['From'] = email['from']
-    msg['To'] = email['to']  # This can be a list of str
-    msg.set_content(email['body'])
+    msg["Subject"] = email["subject"]
+    msg["From"] = email["from"]
+    msg["To"] = email["to"]  # This can be a list of str
+    msg.set_content(email["body"])
     return msg
 
 
@@ -72,26 +77,25 @@ def smtp_reporter():
     smtp = SMTP(host=config.smtp_host, port=config.smtp_port)
 
     def on_message(message: Message):
-        logger.debug('From queue: %s, received: %s',
-                      config.amqp_queue, message.body)
+        logger.debug("From queue: %s, received: %s", config.amqp_queue, message.body)
         try:
             email = json.loads(message.body)
         except json.JSONDecodeError as err:
-            logger.critical('Cannot decode JSON message: %s', message.body)
+            logger.critical("Cannot decode JSON message: %s", message.body)
             message.reject()
-            logger.debug('Rejected: %s', message.body)
+            logger.debug("Rejected: %s", message.body)
             return
         msg = compose_email(email)
         try:
             smtp.send_message(msg)
         except SMTPException as err:
-            logger.critical('Unable to send email message: %s', message.body)
+            logger.critical("Unable to send email message: %s", message.body)
             message.requeue()
-            logger.debug('Requeued: %s', message.body)
+            logger.debug("Requeued: %s", message.body)
             return
-        logger.debug('Email sent: %s', message.body)
+        logger.debug("Email sent: %s", message.body)
         message.ack()
-        logger.debug('Acked: %s', message.body)
+        logger.debug("Acked: %s", message.body)
 
     try:
         yield on_message
@@ -100,28 +104,31 @@ def smtp_reporter():
 
 
 def sigint_handler(_signum, _frame):
-    logger.info('Received SIGINT. Terminating.')
+    logger.info("Received SIGINT. Terminating.")
     hub.stop()
 
 
 def sigterm_handler(_signum, _frame):
-    logger.info('Received SIGTERM. Terminating.')
+    logger.info("Received SIGTERM. Terminating.")
     hub.stop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     signal.signal(signal.SIGINT, sigint_handler)
     signal.signal(signal.SIGTERM, sigterm_handler)
     conn.register_with_event_loop(hub)
 
-    logger.info('Configuration: %s', config)
-    if config.reporter_type == 'elasticsearch':
+    logger.info("Configuration: %s", config)
+    if config.reporter_type == "elasticsearch":
         report = es_reporter()
-    elif config.reporter_type == 'email':
+    elif config.reporter_type == "email":
         report = smtp_reporter()
     with report as on_message_report:
-        with Consumer(conn, [queue],
-                      prefetch_count=config.amqp_prefetch,
-                      on_message=on_message_report):
-            logger.info('Starting main loop')
+        with Consumer(
+            conn,
+            [queue],
+            prefetch_count=config.amqp_prefetch,
+            on_message=on_message_report,
+        ):
+            logger.info("Starting main loop")
             hub.run_forever()
